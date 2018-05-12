a7dad437774c674cb2d6b675e80e5cfeea33584b
Sanjeev Saroy <saroy94@gmail.com>
2018-05-12 13:05:27 +0100

    Render the player onto the board

--- js/app.js
+++ js/app.js
@@ -24,12 +24,22 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+
+    this.sprite = 'images/char-boy.png';
+    this.x = 202;
+    this.y = 385;
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
@@ -42,5 +52,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    // player.handleInput(allowedKeys[e.keyCode]);
 });
