025537fe7c38b73b35452d7ee0bbcbe0c9de13fa
Sanjeev Saroy <saroy94@gmail.com>
2018-05-13 17:52:38 +0100

    Move enemies across the board

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ var Enemy = function() {
     let startingPosition = getRandomStartingPosition();
     this.x = startingPosition.x;
     this.y = startingPosition.y;
+    this.speed = Math.floor(Math.random() * 500) + 100;
 };
 
 // Update the enemy's position, required method for game
@@ -30,6 +31,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += (this.speed * dt);
 };
 
 // Draw the enemy on the screen, required method for game
